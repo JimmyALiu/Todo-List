b83b53d0d795c80d20d22c0f6553d651bef09016
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-04 17:36:31 -0800

    Fixed imports

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoInput"
 
+function App() {
   return (
-    <>
-      
-    </>
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
   )
 }
 
