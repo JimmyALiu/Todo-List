38549ec80bf6e152ed6edc41f8e57a33189519c2
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-04 18:32:42 -0800

    Moving the todo-list to parent element

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -2,10 +2,16 @@ import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
+	let todos = [
+        'Go to the gym',
+        'Eat healthier'
+    ]
+
+
 	return (
 		<>
-			<TodoInput />
-			<TodoList />
+			<TodoInput todos={todos} />
+			<TodoList todos={todos} />
 		</>
 	)
 }
